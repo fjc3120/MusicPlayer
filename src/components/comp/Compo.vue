c5f848f659b3526287cfe561eb3604457de41596
fjc3120 <287473791@qq.com>
2018-08-09 16:34:59 +0800

    c

--- src/components/comp/Compo.vue
+++ src/components/comp/Compo.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
     <div class="header">
+      <img src="//y.gtimg.cn/mediastyle/yqq/img/logo@2x.png?nowebp=1 2x" alt="" class="qqlogo">
       <span class="header-title">音乐播放器</span>
     </div>
     <div class="footer">
       <ul class="footer-title">
-        <li @click="routerLink('/')"><span  class="title-left">推荐</span></li>
-        <li @click="routerLink('list')"><span  class="title-right">排行榜</span></li>
+        <li @click="routerLink('/')" ><span  class="title-left" :class="{classa:flag}">推荐</span></li>
+        <li @click="routerLink('list')"><span  class="title-right" :class="{classb:!flag}">排行榜</span></li>
       </ul>
+      <div :class="{classA:flag}"></div>
+      <div :class="{classB:!flag}"></div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default({
+
+  data(){
+      return{
+      flag:true
+      }
+  },
   methods:{
     routerLink(path){
+      this.flag = !this.flag;
         this.$router.push(path);
-    }
+    },
+
+
   }
+
 })
 
 </script>
@@ -26,16 +40,16 @@ export default({
 
 <style scoped>
   .header{
-    background: #0000ff;
+    background: #0fff;
     height:50px;
     position: fixed;
     top:0;
     left: 0;
     width:100%;
     color: white;
+    color: #000;
   }
   .footer{
-    background: #0fff;
     height:50px;
     position: fixed;
     top:50px;
@@ -57,5 +71,30 @@ export default({
     line-height: 50px;
 
   }
-
+  .qqlogo{
+    width: 80px;
+    height: 20px;
+    position: absolute;
+    top:50%;
+    transform:translate(50%,-50%);
+  }
+  .classA{
+    height: 4px;
+    width:50%;
+    background: green;
+    position: absolute;
+  }
+  .classB{
+    height: 4px;
+    width:50%;
+    background: green;
+    position: absolute;
+    right: 0;
+  }
+  .classa{
+    color: green;
+  }
+  .classb{
+    color: green;
+  }
 </style>
--- src/components/recommend/Recommend.vue
+++ src/components/recommend/Recommend.vue
@@ -1,10 +1,54 @@
 <template>
-<h1>Recommend</h1>
+<div>
+  <swipe class="my-swipe">
+    <swipe-item class="slide1"><img src="@/assets/img/1.jpg" alt=""></swipe-item>
+    <swipe-item class="slide2"><img src="@/assets/img/2.jpg" alt=""></swipe-item>
+    <swipe-item class="slide3"><img src="@/assets/img/3.jpg" alt=""></swipe-item>
+    <swipe-item class="slide3"><img src="@/assets/img/4.jpg" alt=""></swipe-item>
+    <swipe-item class="slide3"><img src="@/assets/img/5.jpg" alt=""></swipe-item>
+  </swipe>
+  <div><img src="@/assets/img/f.png" alt=""></div>
+  <hr>
+  <div class="dt">歌单推荐 </div>
+  <hr>
+  <div class="box">
+    <ul>
+      <li @click="routerLink('music')"><div><img src="https://y.gtimg.cn/music/photo/radio/track_radio_307_13_1.jpg?max_age=2592000" alt=""><span>123456789</span></div></li>
+      <li @click="routerLink('music')"><div><img src="https://y.gtimg.cn/music/photo/radio/track_radio_199_13_1.jpg?max_age=2592000" alt=""><span>123456789</span></div></li>
+      <li @click="routerLink('music')"><div><img src="https://y.gtimg.cn/music/photo_new/T006R300x300M00000333So02drvak.jpg?max_age=2592000" alt=""><span>123456789</span></div></li>
+      <li @click="routerLink('music')"><div><img src="https://y.gtimg.cn/music/photo_new/T006R300x300M0000013j8zs1jRnLQ.jpg?max_age=2592000" alt=""><span>123456789</span></div></li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
+  require('vue-swipe/dist/vue-swipe.css');
+  import { Swipe, SwipeItem } from 'vue-swipe';
   export default({
-
+    components: {
+      'swipe': Swipe,
+      'swipe-item': SwipeItem
+    },
+    methods: {
+      routerLink(path){
+        this.$router.push(path);
+      }
+    }
+//    methods:{
+//        getData(){
+//            axios.get('https://bird.ioliu.cn/netease?url=http://s.plcloud.music.qq.com/fcgi-bin/fcg_yqq_song_detail_info.fcg?songmid={$SongMid}')
+//              .then(function (response) {
+//                console.log(response);
+//              })
+//              .catch(function (error) {
+//                console.log(error);
+//              });
+//        }
+//    },
+//    created(){
+//      getData();
+//    }
   })
 
 </script>
@@ -12,4 +56,31 @@
 
 <style scoped>
 
+  .my-swipe {
+    height: 140px;
+    width: 100%;
+    font-size: 30px;
+    text-align: center;
+  }
+  .my-swipe img{
+    height: 100%;
+    width: 100%;
+  }
+  .dt{
+    border-left: 3px solid red;
+  }
+  .box{
+    width:100%;
+  }
+  .box ul{
+    width:100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .box li{
+    width:40%;
+    margin: 10px 10px 10px 10px;
+  }
 </style>
